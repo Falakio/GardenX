8ebeeb02ccab076c4e6fc8189571712c1cbca331
Aman Sanoj <amansanoj08@gmail.com>
2024-12-09 17:46:21 +0400

    v1.2.4

--- supabase/profiles.sql
+++
@@ -1,9 +0,0 @@
-create table
-  public.profiles (
-    id uuid not null,
-    email text null,
-    created_at timestamp with time zone not null default timezone ('utc'::text, now()),
-    updated_at timestamp with time zone not null default timezone ('utc'::text, now()),
-    constraint profiles_pkey primary key (id),
-    constraint profiles_id_fkey foreign key (id) references auth.users (id) on delete cascade
-  ) tablespace pg_default;
